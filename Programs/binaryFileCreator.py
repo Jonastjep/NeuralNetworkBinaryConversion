79f3ec223ffbeb7c1de1fe69cd6ceeacb7166401
Jonas Tjepkema <jonas@tjepkema.com>
2018-03-29 00:15:41 +0200

    Changes the file creator to make it more adjustable
    This change allows to create binary numbers with as many bits as you wish, compared to the only three bits it allowed before.
    This gives more room to test the neural network in all shapes and forms, with different nb of input and output neurones.

--- Programs/binaryFileCreator.py
+++ Programs/binaryFileCreator.py
@@ -1,8 +1,26 @@
+#This program creates a txt file that contains the training data for the Neural Network(NN).
+#The data is a random binary number and the neurone that should fire.
+#The variable nbBin represent the number of binary characters you want your binary number to
+#have. This is the only variable you have to modify to change the output in your file,
+#anything else is asked when you run the program.
+#The format of the file is important for the good function of the NN so please don't alter it.
+#The output file has the total number of training cycles as first line and then starts the training
+#data : first the binary form of the number and then the representation of the correct neurone that
+#should fire (represented by the 1, and 0 representing a neurone that shouldn't fire). This representation
+#has 2^nbBin numbers
+#Example : I have the decimal number 7 with a nbBin = 4
+#My first line of data would be 0111 (the binary form of the decimal 7, with 4 binary slots)
+#The second line would be 0000000100000000 (the 1 is on the 8th slot on 16, representing the decimal nb 7)
+#						 (0123456789111111)(these lines are not in the file, they just name the different slots)
+#						 (		    012345)
+#This repeats the number of times you want, which is asked inside the program.
+
+
 import random
 
 def main():
 	nbLine = int(input('Please enter the number of numbers you want: '))
-	nbBin = 3
+	nbBin = 6
 	a = ''
 	
 	printBin(nbLine, nbBin, a)
@@ -16,37 +34,19 @@ def printBin(nbLine, nbBin, a):
 	
 	for i in range(nbLine):
 		for j in range(nbBin):
-			a = a + str(round(random.random()))
+			a = a + str(round(random.random()))							#Creates a random binary number to solve
 		
 		F.write(a)
 		F.write('\n')
-		dec = int(a,2)
+		dec = int(a,2)													#Creates the decimal version of the binary number
 		
-		if(dec == 0):
-			F.write('10000000')
-			F.write('\n')
-		elif(dec == 1):
-			F.write('01000000')
-			F.write('\n')
-		elif(dec == 2):
-			F.write('00100000')
-			F.write('\n')
-		elif(dec == 3):
-			F.write('00010000')
-			F.write('\n')
-		elif(dec == 4):
-			F.write('00001000')
-			F.write('\n')
-		elif(dec == 5):
-			F.write('00000100')
-			F.write('\n')
-		elif(dec == 6):
-			F.write('00000010')
-			F.write('\n')
-		elif(dec == 7):
-			F.write('00000001')
-			F.write('\n')
-		
+		for i in range(pow(2,nbBin)):									#Creates a string of 2^nbBin digits that show to the Neural
+			if i == dec: 												#Network the correct neurone to fire on the final layer.
+				F.write('1')											#The string has the same nb of chars as the final layer has
+			else:														#neurones. The 1 represents the neurone that should have fired,
+				F.write('0')											#the 0 shows that the neurone shouldn't have fired.
+				
+		F.write('\n')
 		a = ''
 	F.close()
 
